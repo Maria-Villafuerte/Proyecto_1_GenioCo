8c04ae5fd856ed6c8c05e7732de3f89c810100e9
Maria-Villafuerte <100331510+Maria-Villafuerte@users.noreply.github.com>
2023-10-27 12:27:59 -0600

    Creación de pantalla Parciales

---
+++ Proyecto_1/app/src/main/java/com/example/proyecto_1/models/Parciales.kt
@@ -0,0 +1,7 @@
+package com.example.proyecto_1.models
+
+import com.example.proyecto_1.R
+
+data class Parciales(var Nombre:String = "Nombre Clase",
+                     val temas: List<Temas> = listOf())
+
